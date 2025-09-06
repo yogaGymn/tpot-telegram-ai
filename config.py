83c2b27bb370eda28acdeaad4d5d5e7191034910
Agus FROST <93752936+AgusFROST@users.noreply.github.com>
2025-09-06 11:15:46 +0700

    Update elastic account

--- config.py
+++ config.py
@@ -1,8 +1,7 @@
 # config.py
-ES_HOST = "http://165.22.249.215"
-ES_USER = "root"
-ES_PASS = "THEvps123!a"
+ES_HOST = "http://localhost:9200"
+ES_USER = "elastic"
+ES_PASS = "combeartni25#"
 
 TELEGRAM_BOT_TOKEN = "8318373099:AAEglaxhxI9E-8J8jM30q5DUIBTx_0rcynU"
 TELEGRAM_CHAT_ID = "5507535048"
-
